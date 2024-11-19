f1a9611d7d6e0cdb85fe30b79a9d724293d3c324
“Jignesh <jigneshtrivedi365@gmail.com>
2024-11-19 19:34:51 +0530

    First Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import './global/store.jsx'
+import { Todo } from './component/Todo.jsx'
+
+function App() {
+  return (
+    <Todo />
+  )
+}
+
+export default App
---
+++ src/global/store.jsx
@@ -0,0 +1,50 @@
+import {createStore} from 'redux'
+
+/* eslint-disable no-case-declarations */
+const initialState = { 
+    task:[]
+}
+// constant declaretion 
+const ADD_TASK = "task/add";
+const DEL_TASK = "task/delete";
+
+const taskReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_TASK:
+            return {
+                ...state,
+                task: [...state.task,action.payload],
+            }
+
+        case DEL_TASK:
+            const updateTask = state.task.filter((task,index) => {
+                return index !== action.payload;
+            });
+            return {
+                ...state,
+                task: [...state.task, updateTask],
+            }
+        default:
+            return state;
+    }
+};
+
+const addTask = (data) => {
+    return {type:ADD_TASK,payload:data};
+}
+
+const delTask = (id) => {
+    return {type:DEL_TASK,payload:id}; 
+}
+
+export const store = createStore(taskReducer)
+console.log("Initial Store  :", store.getState());
+
+store.dispatch(addTask("Hello world"))
+store.dispatch(addTask("Buy now technical code"))
+
+console.log("store after dispacth Add : ",store.getState());
+
+store.dispatch(delTask(1))
+
+console.log("store after dispacth Dele : ",store.getState());
